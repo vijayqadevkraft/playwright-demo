d79f52e63f4d97fce01d85112e5df1864054851b
vijayqadevkraft <95744146+vijayqadevkraft@users.noreply.github.com>
2025-11-17 15:31:25 +0530

    Create playwright.config.js

---
+++ playwright.config.js
@@ -0,0 +1,59 @@
+// @ts-check
+const { defineConfig, devices } = require('@playwright/test');
+
+module.exports = defineConfig({
+  testDir: './tests',
+  
+  // Maximum time one test can run for
+  timeout: 30 * 1000,
+  
+  expect: {
+    // Maximum time expect() should wait for the condition to be met
+    timeout: 5000
+  },
+  
+  // Run tests in files in parallel
+  fullyParallel: true,
+  
+  // Fail the build on CI if you accidentally left test.only in the source code
+  forbidOnly: !!process.env.CI,
+  
+  // Retry on CI only
+  retries: process.env.CI ? 2 : 0,
+  
+  // Opt out of parallel tests on CI
+  workers: process.env.CI ? 1 : undefined,
+  
+  // Reporter to use
+  reporter: 'html',
+  
+  // Shared settings for all the projects below
+  use: {
+    // Base URL to use in actions like `await page.goto('/')`
+    // baseURL: 'http://localhost:3000',
+    
+    // Collect trace when retrying the failed test
+    trace: 'on-first-retry',
+    
+    // Screenshot on failure
+    screenshot: 'only-on-failure',
+  },
+  
+  // Configure projects for major browsers
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+    
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
+  ],
+});
